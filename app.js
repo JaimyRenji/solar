00242f5f9c2cf50917afadac93db46c03172634d
JaimyRenji <116898063+JaimyRenji@users.noreply.github.com>
2026-09-25 13:57:25 +0530

    Change MongoDB connection to hardcoded string
    Updated MongoDB connection string to a hardcoded value.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect("mongodb+srv://superuser:superpass@supercluster.d83jj.mongodb.net/superData", {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
     useNewUrlParser: true,
